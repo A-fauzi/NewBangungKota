ec514b737607a44afd1fd8fe94b5258634d6a980
A-fauzi <akhmadfauzi0101@gmail.com>
2023-09-05 19:26:19 +0700

    set data toolbar from current user

--- app/src/main/java/com/afauzi/bangungkota/presentation/ui/main/fragment/HomeFragment.kt
+++ app/src/main/java/com/afauzi/bangungkota/presentation/ui/main/fragment/HomeFragment.kt
@@ -1,22 +1,61 @@
 package com.afauzi.bangungkota.presentation.ui.main.fragment
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.afauzi.bangungkota.R
+import com.afauzi.bangungkota.databinding.FragmentHomeBinding
+import com.afauzi.bangungkota.utils.CustomViews
+import com.afauzi.bangungkota.utils.CustomViews.toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        binding.appBarLayout.topAppBar.title = "Hi ${user?.displayName} 🙌"
+        Glide.with(this)
+            .asBitmap()
+            .load(user?.photoUrl.toString())
+            .apply(RequestOptions.circleCropTransform())
+            .into(object : CustomTarget<Bitmap>(){
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    val iconDrawable = BitmapDrawable(resources, resource)
+                    binding.appBarLayout.topAppBar.menu.findItem(R.id.user).icon = iconDrawable
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                    // this is called when imageView is cleared on lifecycle call or for
+                    // some other reason.
+                    // if you are referencing the bitmap somewhere else too other than this imageView
+                    // clear it here as you can no longer have the bitmap
+                }
+
+            })
+    }
 }
