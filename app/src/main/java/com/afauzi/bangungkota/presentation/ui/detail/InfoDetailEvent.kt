9661141f6be099000cc63d30e2d2c01ded5576a2
A-fauzi <akhmadfauzi0101@gmail.com>
2023-09-07 04:02:20 +0700

    set data to view detail

--- app/src/main/java/com/afauzi/bangungkota/presentation/ui/detail/InfoDetailEvent.kt
+++ app/src/main/java/com/afauzi/bangungkota/presentation/ui/detail/InfoDetailEvent.kt
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.afauzi.bangungkota.R
 import com.afauzi.bangungkota.databinding.ActivityInfoDetailEventBinding
 import com.afauzi.bangungkota.domain.model.Event
 import com.afauzi.bangungkota.presentation.viewmodels.UserViewModel
+import com.afauzi.bangungkota.utils.UtilityLibrary
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class InfoDetailEvent : AppCompatActivity() {
 
     private lateinit var binding: ActivityInfoDetailEventBinding
+    private lateinit var auth: FirebaseAuth
+    private var user: FirebaseUser? = null
     private val userViewModel: UserViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +29,62 @@ class InfoDetailEvent : AppCompatActivity() {
         binding = ActivityInfoDetailEventBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+        user = auth.currentUser
+
+        if (user != null) {
+            UtilityLibrary.imageGlideForCircle(
+                context = this,
+                dataImg = user?.photoUrl.toString(),
+                res = resources
+            ) { bitmap ->
+                binding.toolAppBar.topAppBar.menu.findItem(R.id.user).icon = bitmap
+            }
+        }
+
+        binding.toolAppBar.topAppBar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
         val receivedData = intent.getParcelableExtra("event_data") as? Event
         if (receivedData != null) {
-            Toast.makeText(this, receivedData.id, Toast.LENGTH_SHORT).show()
+
+            lifecycleScope.launch {
+                userViewModel.getUserById(receivedData.createdBy)
+                    .addOnSuccessListener {
+                        if (it.exists()) {
+
+                            Glide.with(this@InfoDetailEvent)
+                                .load(it.getString("photo"))
+                                .into(binding.ivProfile)
+
+                            binding.tvUsername.text = it.getString("name")
+                            binding.tvUserEmail.text = it.getString("email")
+                            binding.tvUserAddress.text = receivedData.address
+                            Glide.with(this@InfoDetailEvent)
+                                .load(receivedData.image)
+                                .into(binding.ivEvent)
+                            binding.tvTitleEvent.text = receivedData.title
+                            binding.tvDateEvent.text = receivedData.date
+                            binding.tvDescEvent.text = receivedData.description
+
+                        } else {
+                            Toast.makeText(
+                                this@InfoDetailEvent,
+                                "Data tidak ditemukan!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(
+                            this@InfoDetailEvent,
+                            "error fetch user ${it.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+            }
+
         }
     }
 }
