010e3aae5e363fad1444e0a4113c66d30bf7cbe5
A-fauzi <akhmadfauzi0101@gmail.com>
2023-09-06 18:43:06 +0700

    handle delete post

--- app/src/main/java/com/afauzi/bangungkota/presentation/ui/main/fragment/CommunityFragment.kt
+++ app/src/main/java/com/afauzi/bangungkota/presentation/ui/main/fragment/CommunityFragment.kt
@@ -24,6 +24,7 @@ import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -42,57 +43,11 @@ class CommunityFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentCommunityBinding.inflate(layoutInflater, container, false)
 
-        adapterPagingPost = AdapterPagingPost{componentListCommunityPostBinding, post ->
+        adapterPagingPost = AdapterPagingPost { componentListCommunityPostBinding, post ->
             bindDataPostToViews(componentListCommunityPostBinding, post)
         }
 
         return binding.root
-    }
-
-    private fun bindDataPostToViews(
-        componentListCommunityPostBinding: ComponentListCommunityPostBinding,
-        post: Post
-    ) {
-        val db = FirebaseFirestore.getInstance()
-        val auth = FirebaseAuth.getInstance()
-        val user = auth.currentUser
-        db.collection("users").document(post.uid.toString()).get()
-            .addOnSuccessListener {
-                if (it.exists()) {
-
-                    if (user?.uid != post.uid) componentListCommunityPostBinding.btnMorePost.visibility = View.GONE
-
-                    componentListCommunityPostBinding.tvTextPost.text = post.text
-                    componentListCommunityPostBinding.itemNameUser.text = it.getString("name")
-                    componentListCommunityPostBinding.itemEmailUser.text = it.getString("email")
-                    Glide.with(requireActivity())
-                        .load(it.getString("photo"))
-                        .error(R.drawable.example_profile)
-                        .into(componentListCommunityPostBinding.itemIvProfile)
-
-                    // GET VALUE AND SET TO ITEM POSTING COMMUNITY
-                    componentListCommunityPostBinding.btnMorePost.setOnClickListener {
-                        // Handle button more post
-                        // BottomSheet
-                        val dialog = BottomSheetDialog(requireActivity())
-                        val sheetBinding = ComponentBottomSheetMorePostBinding.inflate(layoutInflater)
-                        val view = sheetBinding.root
-                        dialog.setContentView(view)
-
-                        val progressBar = sheetBinding.progressbar
-                        val layoutItem = sheetBinding.layoutItem
-                        sheetBinding.btnDelete.setOnClickListener {
-//                            deleteEventPost(progressBar, layoutItem, dialog, post)
-                        }
-                        dialog.show()
-                    }
-                } else {
-                    // tidak ada data uid di list
-                }
-            }
-            .addOnFailureListener {
-                toast(requireActivity(), it.message)
-            }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -102,6 +57,102 @@ class CommunityFragment : Fragment() {
         onClickViews()
         setUpRecyclerViewAdapter()
     }
+
+    private fun bindDataPostToViews(
+        componentListCommunityPostBinding: ComponentListCommunityPostBinding,
+        post: Post
+    ) {
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("users").document(post.uid.toString()).get()
+            .addOnSuccessListener {
+                if (it.exists()) {
+                    setDataListItem(post, componentListCommunityPostBinding, it)
+                } else {
+                    // tidak ada data uid di list
+                }
+            }
+            .addOnFailureListener {
+                toast(requireActivity(), it.message)
+            }
+    }
+
+    private fun setDataListItem(
+        post: Post,
+        componentListCommunityPostBinding: ComponentListCommunityPostBinding,
+        it: DocumentSnapshot
+    ) {
+        val auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        if (user?.uid != post.uid) componentListCommunityPostBinding.btnMorePost.visibility =
+            View.GONE
+
+        componentListCommunityPostBinding.tvTextPost.text = post.text
+        componentListCommunityPostBinding.itemNameUser.text = it.getString("name")
+        componentListCommunityPostBinding.itemEmailUser.text = it.getString("email")
+        Glide.with(requireActivity())
+            .load(it.getString("photo"))
+            .error(R.drawable.example_profile)
+            .into(componentListCommunityPostBinding.itemIvProfile)
+
+
+        var isLoved = false
+        componentListCommunityPostBinding.btnLove.setOnClickListener {
+            val postId = post.id
+            val currentUserId = user?.uid.toString()
+
+            if (isLoved) {
+                componentListCommunityPostBinding.btnLove.setImageResource(R.drawable.heart_outline)
+                toast(requireActivity(), "Batal Menyukai")
+
+                // Handle deleted data like in document
+            } else {
+                componentListCommunityPostBinding.btnLove.setImageResource(R.drawable.heart_filled)
+                toast(requireActivity(), "Kamu Menyukai ini")
+            }
+
+            isLoved = !isLoved
+        }
+
+        // GET VALUE AND SET TO ITEM POSTING COMMUNITY
+        componentListCommunityPostBinding.btnMorePost.setOnClickListener {
+            // Handle button more post
+            // BottomSheet
+            val dialog = BottomSheetDialog(requireActivity())
+            val sheetBinding = ComponentBottomSheetMorePostBinding.inflate(layoutInflater)
+            val view = sheetBinding.root
+            dialog.setContentView(view)
+
+            val progressBar = sheetBinding.progressbar
+            val layoutItem = sheetBinding.layoutItem
+            sheetBinding.btnDelete.setOnClickListener {
+                progressBar.visibility = View.VISIBLE
+                layoutItem.visibility = View.GONE
+
+                // Delete post
+                val db = FirebaseFirestore.getInstance()
+                db.collection("posts").document(post.id).delete()
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            toast(requireActivity(), "Post Deleted 🙌")
+                            adapterPagingPost.refresh()
+                            dialog.dismiss()
+                        } else {
+                            progressBar.visibility = View.GONE
+                            layoutItem.visibility = View.VISIBLE
+                            toast(requireActivity(), "Hufftt gagal Delete post!")
+                        }
+                    }
+                    .addOnFailureListener {
+                        progressBar.visibility = View.GONE
+                        layoutItem.visibility = View.VISIBLE
+                        toast(requireActivity(), "Ada masalah server 😥")
+                    }
+            }
+            dialog.show()
+        }
+    }
+
 
     private fun setUpRecyclerViewAdapter() {
         binding.rvCommunityPost.apply {
@@ -161,7 +212,7 @@ class CommunityFragment : Fragment() {
             .set(data)
             .addOnCompleteListener {
                 if (it.isSuccessful) {
-//                    adapterPagingList.refresh()
+                    adapterPagingPost.refresh()
                     binding.outlineTextfieldProductSpec.isEnabled = true
                     binding.etPostText.text?.clear()
                 } else {
