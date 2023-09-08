33c6473039a4a04132383d1ad32e0a22d3e0e55b
A-fauzi <akhmadfauzi0101@gmail.com>
2023-09-08 18:35:25 +0700

    input behaviour

--- app/src/main/java/com/afauzi/bangungkota/presentation/ui/detail/InfoDetailPostActivity.kt
+++ app/src/main/java/com/afauzi/bangungkota/presentation/ui/detail/InfoDetailPostActivity.kt
@@ -3,6 +3,8 @@ package com.afauzi.bangungkota.presentation.ui.detail
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.lifecycleScope
@@ -12,6 +14,7 @@ import com.afauzi.bangungkota.domain.model.Post
 import com.afauzi.bangungkota.presentation.viewmodels.PostViewModel
 import com.afauzi.bangungkota.presentation.viewmodels.UserViewModel
 import com.bumptech.glide.Glide
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,6 +27,9 @@ class InfoDetailPostActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailPostBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var inputCommentMessageLayout: TextInputLayout
+    private lateinit var inputCommentMessageEdiText: EditText
+
     private var user: FirebaseUser? = null
 
     private val userViewModel: UserViewModel by viewModels()
@@ -32,6 +38,9 @@ class InfoDetailPostActivity : AppCompatActivity() {
     private fun init() {
         auth = FirebaseAuth.getInstance()
         user = auth.currentUser
+
+        inputCommentMessageLayout = binding.inputReply.outlineTextfieldCommentMessage
+        inputCommentMessageEdiText = binding.inputReply.etPostComment
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,8 +50,11 @@ class InfoDetailPostActivity : AppCompatActivity() {
 
         init()
         setDetailDataPost()
+        thisActivityBehaviour()
 
+    }
 
+    private fun thisActivityBehaviour() {
         // Image current user in input column
         Glide.with(this@InfoDetailPostActivity)
             .load(user?.photoUrl)
@@ -50,6 +62,21 @@ class InfoDetailPostActivity : AppCompatActivity() {
             .error(R.drawable.image_error)
             .into(binding.inputReply.ivCurrentUserMessage)
 
+
+        // show if start activity
+        inputBehaviour(inputCommentMessageEdiText, true)
+    }
+
+    private fun inputBehaviour(editTextView: EditText, isShowing: Boolean) {
+        // Window input text down
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if (isShowing) {
+            editTextView.requestFocus()
+            inputMethodManager.showSoftInput( editTextView, InputMethodManager.SHOW_IMPLICIT)
+        } else {
+            inputMethodManager.hideSoftInputFromWindow(editTextView.windowToken, 0)
+        }
     }
 
     private fun setDetailDataPost() {
