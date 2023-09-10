7c85bfb9aa069ea2cd6352dea9d79145423387c8
A-fauzi <akhmadfauzi0101@gmail.com>
2023-09-10 22:40:36 +0700

    add input validation and prefix

--- app/src/main/java/com/afauzi/bangungkota/presentation/ui/detail/InfoDetailPostActivity.kt
+++ app/src/main/java/com/afauzi/bangungkota/presentation/ui/detail/InfoDetailPostActivity.kt
@@ -3,6 +3,8 @@ package com.afauzi.bangungkota.presentation.ui.detail
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.Toast
@@ -12,7 +14,6 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.afauzi.bangungkota.R
 import com.afauzi.bangungkota.databinding.ActivityDetailPostBinding
 import com.afauzi.bangungkota.databinding.ComponentListReplyPostBinding
@@ -21,15 +22,15 @@ import com.afauzi.bangungkota.presentation.adapter.AdapterPagingReplyPost
 import com.afauzi.bangungkota.presentation.viewmodels.PostReplyViewModel
 import com.afauzi.bangungkota.presentation.viewmodels.PostViewModel
 import com.afauzi.bangungkota.presentation.viewmodels.UserViewModel
-import com.afauzi.bangungkota.utils.CustomViews
 import com.afauzi.bangungkota.utils.CustomViews.circularDrawableToLoadInput
+import com.afauzi.bangungkota.utils.CustomViews.toast
 import com.afauzi.bangungkota.utils.UniqueIdGenerator.generateUniqueId
-import com.afauzi.bangungkota.utils.UtilityLibrary
 import com.afauzi.bangungkota.utils.UtilityLibrary.formatedDateToTimeAgo
 import com.bumptech.glide.Glide
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentSnapshot
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -39,17 +40,24 @@ import java.util.*
 @AndroidEntryPoint
 class InfoDetailPostActivity : AppCompatActivity() {
 
+    companion object {
+        const val END_ICON_MODE_SEND = 0
+        const val END_ICON_MODE_CLEAR_PREFIX = 1
+    }
+
     private lateinit var binding: ActivityDetailPostBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var inputCommentMessageLayout: TextInputLayout
     private lateinit var inputCommentMessageEdiText: EditText
     private lateinit var adapterPagingReplyPost: AdapterPagingReplyPost
 
-    private var user: FirebaseUser? = null
-
     private val userViewModel: UserViewModel by viewModels()
     private val postViewModel: PostViewModel by viewModels()
     private val postReplyViewModel: PostReplyViewModel by viewModels()
+
+    private var user: FirebaseUser? = null
+    private var isPrefix: Boolean = false
+    private var endIconMode: Int = 0
 
     private fun init() {
         auth = FirebaseAuth.getInstance()
@@ -58,7 +66,7 @@ class InfoDetailPostActivity : AppCompatActivity() {
         inputCommentMessageLayout = binding.inputReply.outlineTextfieldCommentMessage
         inputCommentMessageEdiText = binding.inputReply.etPostComment
 
-        adapterPagingReplyPost = AdapterPagingReplyPost {viewBind, dataReply ->
+        adapterPagingReplyPost = AdapterPagingReplyPost { viewBind, dataReply ->
             getUserReplyAndSetDataBinding(dataReply, viewBind)
         }
 
@@ -75,15 +83,17 @@ class InfoDetailPostActivity : AppCompatActivity() {
 
         setDetailDataPost { post ->
 
-            if (post.uid.toString() != user?.uid.toString()) binding.itemPost.btnMorePost.isVisible = false
+            inputPrefixValidation(post)
+
+
+            if (post.uid.toString() != user?.uid.toString()) binding.itemPost.btnMorePost.isVisible =
+                false
 
             // Get user live data
             userDetailPostLiveData(post.uid.toString())
 
             binding.itemPost.tvTextPost.text = post.text
             binding.itemPost.itemDatePost.text = formatedDateToTimeAgo(post.created_at)
-
-            onClickMessageReply(post)
 
             lifecycleScope.launch {
                 postReplyViewModel.getReplyPost(post.id).collectLatest { pagingData ->
@@ -93,9 +103,14 @@ class InfoDetailPostActivity : AppCompatActivity() {
         }
 
         binding.rvReply.apply {
-            layoutManager = LinearLayoutManager(this@InfoDetailPostActivity, LinearLayoutManager.VERTICAL, false)
+            layoutManager = LinearLayoutManager(
+                this@InfoDetailPostActivity,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
             adapter = adapterPagingReplyPost
         }
+
 
     }
 
@@ -108,13 +123,28 @@ class InfoDetailPostActivity : AppCompatActivity() {
 
         lifecycleScope.launch {
             userViewModel.getUserById(dataReply.userId.toString())
-                .addOnSuccessListener {
-                    if (it.exists()) {
+                .addOnSuccessListener { user ->
+                    if (user.exists()) {
                         Glide.with(this@InfoDetailPostActivity)
-                            .load(it.getString("photo"))
+                            .load(user.getString("photo"))
                             .into(viewBind.replyPostParent.itemIvProfile)
 
-                        viewBind.replyPostParent.itemNameUser.text = it.getString("name")
+                        viewBind.replyPostParent.itemNameUser.text = user.getString("name")
+
+                        viewBind.replyPostParent.btnComment.setOnClickListener {
+                            isPrefix = true
+                            inputCommentMessageLayout.prefixText = "@${user.getString("name")}"
+
+                            if (inputCommentMessageEdiText.text.toString().isBlank()) {
+                                endIconMode = END_ICON_MODE_CLEAR_PREFIX
+                                inputCommentMessageLayout.endIconDrawable =
+                                    ContextCompat.getDrawable(
+                                        this@InfoDetailPostActivity,
+                                        R.drawable.cross_circle
+                                    )
+                            }
+                        }
+
                     } else {
 
                     }
@@ -123,6 +153,69 @@ class InfoDetailPostActivity : AppCompatActivity() {
 
                 }
         }
+    }
+
+    private fun inputPrefixValidation(post: Post) {
+
+        // OnClick send message reply
+        inputCommentMessageLayout.setEndIconOnClickListener {
+
+            if (endIconMode == END_ICON_MODE_SEND) {
+                if (isPrefix && inputCommentMessageEdiText.text.toString().isNotBlank()) toast(
+                    this,
+                    "reply child"
+                )
+
+                if (!isPrefix) {
+
+                    if (inputCommentMessageEdiText.text.toString().isNotBlank()) {
+                        createReplyPost(post)
+                    } else {
+                        toast(this, "minimal isi dulu 🤦‍♀️")
+                    }
+
+                }
+            }
+
+            if (endIconMode == END_ICON_MODE_CLEAR_PREFIX) {
+                inputCommentMessageLayout.endIconDrawable = ContextCompat.getDrawable(
+                    this@InfoDetailPostActivity,
+                    R.drawable.ic_paper_plane_top
+                )
+                inputCommentMessageLayout.prefixText = null
+                isPrefix = false
+                endIconMode = END_ICON_MODE_SEND
+            }
+
+            inputBehaviour(inputCommentMessageEdiText, false)
+            inputCommentMessageEdiText.text.clear()
+        }
+
+        inputCommentMessageEdiText.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                val text = p0.toString()
+
+                if (isPrefix) {
+                    if (text.isNotBlank()) {
+                        endIconMode = END_ICON_MODE_SEND
+                        inputCommentMessageLayout.endIconDrawable = ContextCompat.getDrawable(
+                            this@InfoDetailPostActivity,
+                            R.drawable.ic_paper_plane_top
+                        )
+                    } else {
+                        endIconMode = END_ICON_MODE_CLEAR_PREFIX
+                        inputCommentMessageLayout.endIconDrawable = ContextCompat.getDrawable(
+                            this@InfoDetailPostActivity,
+                            R.drawable.cross_circle
+                        )
+                    }
+                }
+            }
+
+        })
     }
 
     private fun thisActivityBehaviour() {
@@ -144,7 +237,7 @@ class InfoDetailPostActivity : AppCompatActivity() {
 
         if (isShowing) {
             editTextView.requestFocus()
-            inputMethodManager.showSoftInput( editTextView, InputMethodManager.SHOW_IMPLICIT)
+            inputMethodManager.showSoftInput(editTextView, InputMethodManager.SHOW_IMPLICIT)
         } else {
             inputMethodManager.hideSoftInputFromWindow(editTextView.windowToken, 0)
         }
@@ -160,20 +253,12 @@ class InfoDetailPostActivity : AppCompatActivity() {
         }
     }
 
-    private fun onClickMessageReply(post: Post) {
-        inputCommentMessageLayout.setEndIconOnClickListener {
-            val circularProgressDrawable = circularDrawableToLoadInput(this)
-            inputCommentMessageLayout.endIconDrawable = circularProgressDrawable
-            circularProgressDrawable.start()
+    private fun createReplyPost(post: Post) {
 
-            viewModels(post)
+        val circularProgressDrawable = circularDrawableToLoadInput(this)
+        inputCommentMessageLayout.endIconDrawable = circularProgressDrawable
+        circularProgressDrawable.start()
 
-            inputBehaviour(inputCommentMessageEdiText, false)
-            inputCommentMessageEdiText.text.clear()
-        }
-    }
-
-    private fun viewModels(post: Post) {
         val data = Post.ReplyPost(
             id = generateUniqueId(),
             postId = post.id,
@@ -187,15 +272,18 @@ class InfoDetailPostActivity : AppCompatActivity() {
                 if (it.isSuccessful) {
                     adapterPagingReplyPost.refresh()
 
-                    inputCommentMessageLayout.endIconDrawable = ContextCompat.getDrawable(this, R.drawable.ic_paper_plane_top)
+                    inputCommentMessageLayout.endIconDrawable =
+                        ContextCompat.getDrawable(this, R.drawable.ic_paper_plane_top)
                 } else {
                     Toast.makeText(this, "reply not send", Toast.LENGTH_SHORT).show()
-                    inputCommentMessageLayout.endIconDrawable = ContextCompat.getDrawable(this, R.drawable.ic_paper_plane_top)
+                    inputCommentMessageLayout.endIconDrawable =
+                        ContextCompat.getDrawable(this, R.drawable.ic_paper_plane_top)
                 }
             }
             .addOnFailureListener {
                 Toast.makeText(this, "error ${it.message}", Toast.LENGTH_SHORT).show()
-                inputCommentMessageLayout.endIconDrawable = ContextCompat.getDrawable(this, R.drawable.ic_paper_plane_top)
+                inputCommentMessageLayout.endIconDrawable =
+                    ContextCompat.getDrawable(this, R.drawable.ic_paper_plane_top)
             }
     }
 
